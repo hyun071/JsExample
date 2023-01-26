34b7a3cdab5c97adedb20775ee4c87c7e6e7ecdc
hyun071 <rhdclsla@gmail.com>
2023-01-26 19:43:54 +0900

    day 5, level2 완

--- tab.js
+++ tab.js
@@ -1,17 +1,17 @@
 
-var tab =  $('.tab-button').length ;
-function tabButton(t){
-    $('.tab-button').removeClass('orange');    
-    $('.tab-button').eq(t).addClass('orange');    
-    $('.tab-content').removeClass('show');    
-    $('.tab-content').eq(t).addClass('show');    
+var tab = $('.tab-button').length;
+function tabButton(t) {
+    $('.tab-button').removeClass('orange');
+    $('.tab-button').eq(t).addClass('orange');
+    $('.tab-content').removeClass('show');
+    $('.tab-content').eq(t).addClass('show');
 }
 // for (let i = 0; i < tab; i++){
 //     $('.tab-button').eq(i).on('click', function(){
 //         tabButton(i)
 //     })
 // }
-$('.list').click(function(e){
+$('.list').click(function (e) {
     tabButton(e.target.dataset.id);
     // if(e.target == document.querySelectorAll('.tab-button')[0]){
     //     tabButton(0)
@@ -20,5 +20,4 @@ $('.list').click(function(e){
     // }    if(e.target == document.querySelectorAll('.tab-button')[2]){
     //     tabButton(2)
     // }
-
 })
