7a5b2aabd3788fe7c85d5debf26f1fe9d9988a25
hyun071 <rhdclsla@gmail.com>
2023-03-25 16:59:08 +0900

    추가내용

--- 3Level.js
+++ 3Level.js
@@ -1,9 +1,28 @@
 // 철수찾기
 var 출석부 = ['흥민', '영희', '철수', '재석'];
 
-function 이름찾기(){
-    console.log('hi');
+function nameSearch(name){
+    for (var i = 0; i < 4; i++) {
+        if (name == 출석부[i]) {
+            console.log('있어요')
+        }
+    }
 }
 
 
-console.log(이름찾기('철수'));
+nameSearch('');
+
+
+
+
+    // 아이디 미입력 혹은 비밀번호가 6자 이하일 시 알림창 띄우고 전송 막기 
+    $('hello').on('submit', function (e) {
+        if (nameSearch().value == '') {
+          alert('있어요');
+          e.preventDefault();
+        } else  {
+          alert('없어요');
+          e.preventDefault();
+        }
+      });
+
